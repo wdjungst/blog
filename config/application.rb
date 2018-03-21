706d7f306bd6ecde8c146ebc885da96d88d28f62
David Jungst <djungst@gmail.com>
2018-03-21 16:05:32 -0600

    whatever

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Blog
   class Application < Rails::Application
+    #meaningless comment
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
